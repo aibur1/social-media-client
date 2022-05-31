ae95e8298381449611b8b814791e4444f1e0eed5
AyuburRahman <aiburrahman17@gmail.com>
2022-05-31 16:47:27 +0600

    adding trend card

---
+++ src/components/TrendCard/TrendCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { TrendData } from '../../Data/TrendData';
+import './TrendCard.css';
+
+const TrendCard = () => {
+  return (
+    <div className="TrendCard">
+        <h3>Trends for you</h3>
+         
+         {TrendData.map((trend)=>{
+             return(
+                 <div className="trend">
+                     <span>#{trend.name}</span>
+                     <span>#{trend.shares}k shares</span>
+                 </div>
+             )
+         })}
+
+    </div>
+  )
+}
+
+export default TrendCard
