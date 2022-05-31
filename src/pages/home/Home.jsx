c9cb61ef5a9cb4ff6635b63a919077fe3d0d0193
AyuburRahman <aiburrahman17@gmail.com>
2022-05-31 14:19:56 +0600

    Initial Commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PostSide from '../../components/PostSide/PostSide';
+import ProfileSide from '../../components/profileSide/ProfileSide';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className="Home">
+        <ProfileSide />
+        <PostSide />
+        <div className="RightSide">Rightside</div>
+    </div>
+  )
+}
+
+export default Home
